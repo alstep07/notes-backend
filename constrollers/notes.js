2aec2d4252917403abbed361df3f26b14dfcb19f
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-04-16 21:00:25 +0300

    refactor: use library for catching errors

--- constrollers/notes.js
+++ constrollers/notes.js
@@ -2,28 +2,20 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 notesRouter.get('/', async (request, response) => {
-  try {
-    const notes = await Note.find({})
-    response.json(notes)
-  } catch (exception) {
-    next(exception)
-  }
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
-  try {
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch (exception) {
-    next(exception)
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   if (!body.content) {
@@ -37,16 +29,12 @@ notesRouter.post('/', async (request, response, next) => {
       date: new Date()
     })
 
-    try {
-      const savedNote = await note.save()
-      response.json(savedNote)
-    } catch (exception) {
-      next(exception)
-    }
+    const savedNote = await note.save()
+    response.json(savedNote)
   }
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const note = {
@@ -54,20 +42,13 @@ notesRouter.put('/:id', (request, response, next) => {
     important: body.important
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote.toJSON())
-    })
-    .catch(error => next(error))
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(updatedNote.toJSON())
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = notesRouter
